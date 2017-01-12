09e5520b64e7bb47c7b9cd2ad8036c179a998c54
Ken Juarez <pokernaut9@gmail.com>
2017-01-12 15:59:18 -0800

    Added code to login in user. This includes form validation and error checking.

--- app/src/main/java/net/devcats/stepit/LoginActivity.java
+++ app/src/main/java/net/devcats/stepit/LoginActivity.java
@@ -1,6 +1,7 @@
 package net.devcats.stepit;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,12 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import net.devcats.stepit.Handlers.UserHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Created by Ken Juarez on 1/12/17.
@@ -45,13 +55,33 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                UserHandler.getInstance().getUser().setId(1);
-                UserHandler.getInstance().saveUser(LoginActivity.this);
+                if (validateForm()) {
+                    try {
+                        JSONObject object = new JSONObject();
 
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                        object.put("email", txtEmailAddress.getText().toString());
+                        object.put("password", txtPassword.getText().toString());
 
-                finish();
+                        new LoginTask().execute(object);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+
+                    // If we fail form validation, should we continue anyways? Since we are in debug?
+                    if (BuildConfig.ENVIRONMENT.equals("debug") && txtEmailAddress.getText().toString().equals("ken9")) {
+
+                        Toast.makeText(LoginActivity.this, "DEBUG MODE: Setting userid to 1", Toast.LENGTH_LONG).show();
+
+                        UserHandler.getInstance().getUser().setId(1);
+                        UserHandler.getInstance().saveUser(LoginActivity.this);
+
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+
+                        finish();
+                    }
+                }
             }
         });
 
@@ -65,6 +95,25 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private boolean validateForm() {
+
+        boolean hasError = false;
+        txtEmailAddress.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+        txtPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(txtEmailAddress.getText()).matches()) {
+            txtEmailAddress.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_attention, 0);
+            hasError = true;
+        }
+
+        if (txtPassword.getText().length() < 4 || txtPassword.getText().length() > 25) {
+            txtPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_attention, 0);
+            hasError = true;
+        }
+
+        return !hasError;
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -74,5 +123,57 @@ public class LoginActivity extends AppCompatActivity {
     private void initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    private class LoginTask extends AsyncTask<JSONObject, Void, String> {
+
+        @Override
+        protected String doInBackground(JSONObject... objects) {
+
+            try {
+                MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+                OkHttpClient client = new OkHttpClient();
+
+                RequestBody body = RequestBody.create(JSON, objects[0].toString());
+                Request request = new Request.Builder()
+                        .url(BuildConfig.LOGIN_URL)
+                        .post(body)
+                        .build();
+                Response response = client.newCall(request).execute();
+
+                return response.body().string();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                JSONObject object = new JSONObject(result);
+
+                if (object.has("error")) {
+                    Toast.makeText(LoginActivity.this, object.getString("error"), Toast.LENGTH_LONG).show();
+                    throw new Exception(object.getString("error"));
+                }
+
+                if (UserHandler.getInstance().parseAndSaveUserFromJSON(LoginActivity.this, object)) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, getString(R.string.error_logging_into_account), Toast.LENGTH_LONG).show();
+                    throw new Exception(getString(R.string.error_logging_into_account));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(LoginActivity.this, getString(R.string.error_logging_into_account), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
